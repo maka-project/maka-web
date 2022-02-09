5ba0c94fe3adaae80b504fd887d46590e7221674
JamesRunnalls <james.runnalls@eawag.ch>
2022-02-09 11:41:18 +0100

    Add favicons

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -4,6 +4,23 @@ import "../../App.css";
 class Home extends Component {
   state = {};
 
+  componentDidMount() {
+    document.getElementById("favicon").href =
+      "https://runnalls.s3.eu-central-1.amazonaws.com/favicon_pending.ico";
+    document.title = "MAKA: Waiting for script connection...";
+
+    setTimeout(function () {
+      document.getElementById("favicon").href =
+        "https://runnalls.s3.eu-central-1.amazonaws.com/favicon_running.ico";
+      document.title = "MAKA: Running Script...";
+    }, 3000);
+    setTimeout(function () {
+      document.getElementById("favicon").href =
+        "https://runnalls.s3.eu-central-1.amazonaws.com/favicon_complete.ico";
+      document.title = "MAKA: Script completed successfully!";
+    }, 6000);
+  }
+
   render() {
     return <div>Hello World</div>;
   }
